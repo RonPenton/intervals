1dc4fda9550431ec8a9492f38978fd993d0aa0a1
Ron Penton <ron.penton@gmail.com>
2025-09-08 20:18:42 -0400

    next

--- src/hill-estimate.ts
+++ src/hill-estimate.ts
@@ -1,13 +1,13 @@
 import 'dotenv-json2/config';
-import { estimateClimbTimeFromPower, estimateClimbTimeFromPowerCurve } from "./estimates";
+import { computeClimbingPower, estimateClimbTimeFromPower, estimateClimbTimeFromPowerCurve } from "./estimates";
 import { getPowerCurve } from "./intervals-api";
 
 const bikeWeightPounds = 20;
 const riderWeightPounds = 400;
 
 
-const hillMiles = 1.2;
-const elevationFeet = 640;
+const hillMiles = 1;
+const elevationFeet = 634;
 const speedMph = 5.3;
 const ftp = 212;
 //const power = 168;
@@ -31,14 +31,16 @@ async function go() {
     const mph = hillMiles / (secondsClimbing / 3600);
     console.log(`Estimated speed at ${power.toFixed(2)} W: ${mph.toFixed(2)} mph`);
 
+    const kph = 9;
+    const mph2 = kph / 1.60934;
 
-    // const power = computeClimbingPower(
-    //     hillMiles,
-    //     elevationFeet,
-    //     speedMph,
-    //     bikeWeightPounds + riderWeightPounds
-    // );
-    // console.log(`Climbing power required: ${power.toFixed(2)} W`);
+    const power2 = computeClimbingPower(
+        hillMiles,
+        elevationFeet,
+        mph2,
+        415
+    );
+    console.log(`Climbing power required: ${power2.toFixed(2)} W`);
 
     // const estPower = 170;
     // const seconds = estimateClimbTimeFromPower(
